8d5511fa722f34d64761fd5a21b42b7af9f2a7c5
Jose Isabel Toledano Morales <Jose.Toledano@axity.com>
2025-02-17 20:32:06 -0600

    Add About page, Gallery component, and footer; update layout and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://joselebasi.github.io',
-    base: 'jardineventosky',
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  site: 'https://joselebasi.github.io',
+  base: 'jardineventosky',
+
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [icon()],
 });
---
+++ src/consts.ts
@@ -0,0 +1,46 @@
+export const ROUTES = [
+  { href: "/", label: "Art" },
+  { href: "/about", label: "About" },
+] as const;
+
+// Icons from https://icon-sets.iconify.design
+export const SOCIAL = [
+  {
+    label: "Twitter",
+    href: "https://twitter.com/EmaSuriano",
+    icon: "mdi:twitter",
+  },
+  {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/emanuel-suriano/",
+    icon: "mdi:linkedin",
+  },
+  {
+    label: "Website",
+    href: "https://emasuriano.com",
+    icon: "mdi:web",
+  },
+] as const;
+
+export const PERSONAL_INFO = {
+  name: "Jose Toledano",
+  title: "Jardin de eventos SKY 🌌",
+  subtitle: "Donde los mejores eventos suceden",
+  role: "Cosmic Digital Artist",
+  contact: "mailto:galactic.canvas@example.com",
+  avatar:
+    "https://github.com/user-attachments/assets/a543b428-3c52-421d-a5d5-32bcd97b1e6e",
+  about: `Step into Galactic Canvas, a realm where the cosmos and creativity collide.
+    Using the power of Astro.js, we bring you an interstellar collection of digital art
+    that transcends the ordinary. Embark on a journey through our curated gallery
+    and experience the future of art in the digital age.`,
+} as const;
+
+export const SEO_INFO = [
+  {
+    name: "description",
+    content: "Art portfolio made by Ema Suriano in his free time",
+  },
+  { name: "keywords", content: "Art portfolio, Digital art, gallery" },
+  { name: "author", content: PERSONAL_INFO.name },
+];
